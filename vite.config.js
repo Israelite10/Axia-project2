861e58cfcb25d1496f33e2838ae19d5b46e53cae
Bello Israel <belloisrael1999@gmail.com>
2025-03-17 05:52:00 -0700

    .

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Axia-project2/', // 👈 Set this to match your GitHub repo name
   plugins: [
     react(),
     tailwindcss(),
   ],
-})
+});
